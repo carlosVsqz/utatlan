0a37348b33efead6252eb1fa7f254ace2fc346dd
Carlos Vasquez <carls10vasquez@gmail.com>
2020-08-27 09:34:08 -0600

    initial commit

---
+++ src/test/java/com/springboot/mgs/utatlan/UtatlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mgs.utatlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtatlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
